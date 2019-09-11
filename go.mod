b16f778a7e157ddcd18f3af06e6d0ee7c3a3f488
nimo@rain-tech.net <nimochu@126.com>
2019-09-11 17:43:21 +0800

    :tada:  request postJSON getJSON

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/og/x
 go 1.13
 
 require (
+	github.com/google/go-querystring v1.0.0
+	github.com/og/x/json v0.0.0-20190911074324-46c1b3e856fb
 	github.com/pkg/errors v0.8.1
 	github.com/stretchr/testify v1.4.0
 )
---
+++ request/post_json.go
@@ -0,0 +1,62 @@
+package greq
+
+import (
+	qs "github.com/google/go-querystring/query"
+	ge "github.com/og/x/error"
+	gjson "github.com/og/x/json"
+	"net/url"
+)
+
+
+type PostJSONProps struct {
+	Path string
+	Query interface{}
+	Data interface{}
+	ResponseDataPtr interface{}
+}
+func PostJSON (props PostJSONProps) (res Response, e Error) {
+	var paramsValue url.Values
+	var err error
+	if props.Query != nil {
+		paramsValue, err = qs.Values(props.Query); ge.Check(err)
+	}
+	res, e = do(Request{
+		Method: "POST",
+		Path: props.Path,
+		Params: paramsValue,
+		Headers: map[string]string {
+			"Content-Type": "application/json",
+		},
+		DataType: Dict().Request.DataType.JSON,
+		JSONData: props.Data,
+	})
+	if !e.Fail {
+		gjson.ParseByte(res.Content, props.ResponseDataPtr)
+	}
+	return
+}
+
+type GetJSONProps struct {
+	Path string
+	Query interface{}
+	ResponseDataPtr interface{}
+}
+func GetJSON (props GetJSONProps) (res Response, e Error) {
+	var paramsValue url.Values
+	var err error
+	if props.Query != nil {
+		paramsValue, err = qs.Values(props.Query); ge.Check(err)
+	}
+	res, e = do(Request{
+		Method: "GET",
+		Path: props.Path,
+		Params: paramsValue,
+		Headers: map[string]string {
+			"Content-Type": "application/json",
+		},
+	})
+	if !e.Fail {
+		gjson.ParseByte(res.Content, props.ResponseDataPtr)
+	}
+	return
+}
