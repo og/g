39981e5f3d4183e411fb72db642fddfe71518297
nimoc <nimocc@126.com>
2020-02-29 22:57:38 +0800

    宽松字典 ，严格字典

--- dict/dict_test.go
+++ dict/dict_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 	"time"
 )
-// 宽松模式的字典
+// 宽松字典
 type Range struct {
 	Type string
 	Start time.Time
@@ -51,9 +51,21 @@ func TestRange(t *testing.T) {
 		is.Equal(text, "1992-12-19 ~ 1992-12-21")
 		is.NoErr(err)
 	}
+	// 宽松字典的缺点是可能会出现
+	query := struct {
+		Type string
+	}{
+		Type: "month",
+	}
+	r.Type = query.Type
+	/*
+	写这段代码的人家假定了 query的字典与 Range 的字典一致
+	但是这种假定是不严谨的，现在写下来的代码可能是一致的，后续如果维护或者修改可能导致
+	query.Type 的值不在 Range 的字典中，从而导致无法预料的错误
+	*/
 }
 
-// 严格模式的字典
+// 严格字典
 type dateTypeDict struct {v string}
 type dateStatusDict struct {v string}
 type Date struct {
@@ -86,4 +98,15 @@ func TestDate(t *testing.T) {
 	is.Equal(r.Type, r.Dict().Type.Day)
 	is.Equal(r.Type.v, "day")
 	is.Equal(r.Status.v, "fail")
+	// 严格字典的优点是不会出现宽松字典的缺点
+	query := struct {
+		Type string
+	}{
+		Type: "month",
+	}
+	_=query
+	// r.Type = query.Type // 赋值会报错
+	r.Type = r.Dict().Type.Day // 只有通过 r.Dict().Type 才能赋值
+	// r.Type = r.Dict().Status.Fail // 字典选错了也会报错（因为 Type 使用的是 dateTypeDict 而 Status使用的是 dateStatusDict
 }
+
