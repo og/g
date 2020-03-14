3040d860934efac82e554805bb4c95acbb31e8b8
Nimo <nimo.jser@gmail.com>
2020-03-14 14:50:31 +0800

    run

--- go.mod
+++ go.mod
@@ -4,6 +4,5 @@ go 1.13
 
 require (
 	github.com/matryer/is v1.2.0
-	github.com/og/x/json v0.0.0-20200228142810-5bec755bfb13
 	github.com/stretchr/testify v1.4.0
 )
---
+++ list/run.go
@@ -0,0 +1,11 @@
+package glist
+
+
+// This is a very simple implementation, see the source code to better understand the role
+func Run(n int, fn func(i int) (_break bool) ) {
+	for i:=0; i<n; i++ {
+		if fn(i) {
+			break
+		}
+	}
+}
